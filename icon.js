8f993d873890ec7d461c436549a002c0833c9c5e
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2013-10-26 01:16:06 +0800

    Initial commit.

---
+++ icon.js
@@ -0,0 +1,23 @@
+
+
+
+function Icon( ){
+
+}
+
+Icon.prototype.boostrap = function bootstrap( ){
+
+};
+
+Icon.prototype.integrate = function integrate( ){
+
+};
+
+Icon.prototype.configure = function configure( ){
+
+};
+
+Icon.prototype.build = function build( ){
+
+};
+
