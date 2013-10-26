5f58085f53ba21c355f438db7435657811994470
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2013-10-26 12:50:04 +0800

    Update iconjs file and added library sub module.

--- icon.js
+++ icon.js
@@ -1,23 +1,30 @@
+define( [ 
+		"angular", 
+		"jquery",
+		"underscore" 
+	],
+	function( ){
 
+		
 
+		function Icon( ){
 
-function Icon( ){
+		}
 
-}
+		Icon.prototype.boostrap = function bootstrap( ){
 
-Icon.prototype.boostrap = function bootstrap( ){
+		};
 
-};
+		Icon.prototype.integrate = function integrate( ){
 
-Icon.prototype.integrate = function integrate( ){
+		};
 
-};
+		Icon.prototype.configure = function configure( ){
 
-Icon.prototype.configure = function configure( ){
+		};
 
-};
+		Icon.prototype.build = function build( ){
 
-Icon.prototype.build = function build( ){
+		};
 
-};
-
+	} );
